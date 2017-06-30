38cfa47e7cbbe72eb44fafe9f925abbec98cde9f
if1live <libsora25@gmail.com>
2017-06-30 15:29:29 +0900

    packging

--- UnityProject/Assets/Minamo/Editor/Menu_PackageExporter.cs
+++ UnityProject/Assets/Minamo/Editor/Menu_PackageExporter.cs
@@ -1,10 +1,76 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
 namespace Assets.Minamo.Editor {
-    class Menu_PackageExporter : ScriptableWizard {
-        [MenuItem("Window/Minamo/Export package")]
-        static void Execute() {
+    class PackageBuilder {
+        public static void Export() {
+            var b = new PackageBuilder();
+            var assets = b.GetAssetPaths();
 
+            string output;
+            if (EnvironmentReader.TryRead("EXPORT_PATH", out output)) {
+                Debug.LogFormat("export package : {0}", output);
+                AssetDatabase.ExportPackage(assets, output);
+            }
+        }
+
+        interface IAssetFinder {
+            string[] GetList();
+        }
+
+        class MinamoAssetFinder : IAssetFinder {
+            public string[] GetList() {
+                var dirs = new string[] {
+                    "Assets/Minamo"
+                };
+                var set = new HashSet<string>();
+                var founds = AssetDatabase.FindAssets("", dirs);
+                foreach (var guid in founds) {
+                    var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                    if (assetPath.EndsWith(".cs")) {
+                        set.Add(assetPath);
+                    }
+                }
+
+                var list = new List<string>(set);
+                return list.ToArray();
+            }
+        }
+
+        public string[] GetAssetPaths() {
+            var finders = new IAssetFinder[] {
+                new MinamoAssetFinder(),
+            };
+
+            var assetPaths = new List<string>();
+            foreach (var f in finders) {
+                assetPaths.AddRange(f.GetList());
+            }
+            return assetPaths.ToArray();
+        }
+    }
+
+    class Menu_PackageExporter : ScriptableWizard {
+        [SerializeField]
+        string[] assets = null;
+
+        [MenuItem("Window/Minamo/Export package")]
+        static void Export() {
+            ScriptableWizard.DisplayWizard("Exporter", typeof(Menu_PackageExporter), "Export");
+        }
+
+        private void OnWizardUpdate() {
+            var b = new PackageBuilder();
+            assets = b.GetAssetPaths();
+        }
+
+        private void OnWizardCreate() {
+            string targetFilePath = EditorUtility.SaveFilePanel("Save package", "", "minamo", "unitypackage");
+            if (targetFilePath == "") {
+                return;
+            }
+            AssetDatabase.ExportPackage(assets, targetFilePath);
         }
     }
 }
---
+++ export_package.bat
@@ -0,0 +1,9 @@
+@set CURR_DIR=%~dp0
+@set PACKAGE_NAME=minamo.unitypackage
+@set UNITY_PATH="C:\\Program Files\\Unity-5.6.1p1\\Editor\\Unity.exe"
+@set PROJ_PATH=%CURR_DIR%\UnityProject
+@set EXPORT_DIR=%CURR_DIR%\dist
+@set EXPORT_PATH=%EXPORT_DIR%\%PACKAGE_NAME%
+@set METHOD=Assets.Minamo.Editor.PackageBuilder.Export
+
+%UNITY_PATH% -quit -batchmode -nographics -silent-crashes -projectPath %PROJ_PATH% -executeMethod %METHOD% -logFile export.log
