d056d403e02f7df7fe76a207679a58f95b3a5b94
if1live <libsora25@gmail.com>
2017-07-14 11:38:36 +0900

    stereo rendering path 추가

--- UnityProject/Assets/Minamo/Editor/Config.cs
+++ UnityProject/Assets/Minamo/Editor/Config.cs
@@ -37,6 +37,11 @@ namespace Assets.Minamo.Editor {
             get { return new AnyDictionary(root.GetList("defines")); }
         }
 
+        AnyDictionary StereoRenderingPath
+        {
+            get { return new AnyDictionary(root.GetDict("stereoRenderingPath")); }
+        }
+
         internal Config(string jsontext) {
             var d = jsontext.FromJson<object>() as Dictionary<string, object>;
             root = new AnyDictionary(d);
@@ -76,6 +81,7 @@ namespace Assets.Minamo.Editor {
                 new ModifierTuple(new Modifier_VRDevice(targetGroup), VRDevices),
                 new ModifierTuple(new Modifier_Keystore(), Keystore),
                 new ModifierTuple(new Modifier_DefineSymbol(targetGroup), Defines),
+                new ModifierTuple(new Modifier_StereoRenderingPath(), StereoRenderingPath),
             };
         }
 
@@ -87,6 +93,7 @@ namespace Assets.Minamo.Editor {
                 new ModifierTuple(Modifier_VRDevice.Current(targetGroup), VRDevices),
                 new ModifierTuple(Modifier_Keystore.Current(), Keystore),
                 new ModifierTuple(Modifier_DefineSymbol.Current(targetGroup), Defines),
+                new ModifierTuple(Modifier_StereoRenderingPath.Current(), StereoRenderingPath),
             };
         }
 
---
+++ UnityProject/Assets/Minamo/Editor/Modifier_StereoRenderingPath.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using UnityEditor;
+
+namespace Assets.Minamo.Editor {
+    class Modifier_StereoRenderingPath : IModifier {
+        StereoRenderingPath stereoRenderingPath;
+
+        public void Apply() {
+            PlayerSettings.stereoRenderingPath = stereoRenderingPath;
+        }
+
+        public string GetConfigText() {
+            var sb = new StringBuilder();
+            sb.AppendFormat("stereoRenderingPath={0}", stereoRenderingPath);
+            return sb.ToString();
+        }
+
+        public void Reload(AnyDictionary dict) {
+            var table = StringEnumConverter.Get<StereoRenderingPath>();
+            stereoRenderingPath = table[dict.GetValue<string>("stereoRenderingPath")];
+        }
+
+        public static Modifier_StereoRenderingPath Current() {
+            return new Modifier_StereoRenderingPath()
+            {
+                stereoRenderingPath = PlayerSettings.stereoRenderingPath,
+            };
+        }
+    }
+}
