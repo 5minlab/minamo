128c2e5fd94a6c54e2dac7f1d04b0792aa711c86
if1live <libsora25@gmail.com>
2017-11-13 15:12:53 +0900

    load와 dump 버튼 위치 변경. create버튼은 한번 누르면 창이 닫히지만 other는 여러번 눌러도 창이 안닫히는점을 이용

--- UnityProject_Minamo/Assets/Minamo/Editor/Menu_ConfigFile.cs
+++ UnityProject_Minamo/Assets/Minamo/Editor/Menu_ConfigFile.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ namespace Assets.Minamo.Editor {
         [MenuItem("Window/Minamo/Show config")]
         static void ShowConfig() {
             if (BaseScriptableWizard.PreExecute()) {
-                ScriptableWizard.DisplayWizard("Uni config file", typeof(Menu_ConfigFile), "Show", "Load");
+                ScriptableWizard.DisplayWizard("Uni config file", typeof(Menu_ConfigFile), "Load", "Dump");
             }
         }
 
@@ -18,6 +18,25 @@ namespace Assets.Minamo.Editor {
         }
 
         private void OnWizardCreate() {
+            LoadButtonClicked();
+        }
+
+        private void OnWizardOtherButton() {
+            DumpButtonClicked();
+        }
+
+        void LoadButtonClicked() {
+            IModifier[] modifiers;
+            PlayerBuildExecutor executor;
+            Load(filePath, out modifiers, out executor);
+
+            foreach (var m in modifiers) {
+                m.Apply();
+            }
+            Debug.LogFormat("Minamo config loaded : {0}", filePath);
+        }
+
+        void DumpButtonClicked() {
             IModifier[] modifiers;
             PlayerBuildExecutor executor;
             Load(filePath, out modifiers, out executor);
@@ -29,17 +48,6 @@ namespace Assets.Minamo.Editor {
                 Debug.LogFormat("{0} : {1}", modifierName, m.GetConfigText());
             }
             Debug.LogFormat("Build : {0}", executor.GetConfigText());
-        }
-
-        private void OnWizardOtherButton() {
-            IModifier[] modifiers;
-            PlayerBuildExecutor executor;
-            Load(filePath, out modifiers, out executor);
-
-            foreach (var m in modifiers) {
-                m.Apply();
-            }
-            Debug.LogFormat("Uni config loaded : {0}", filePath);
         }
 
         void Load(string filepath, out IModifier[] modifiers, out PlayerBuildExecutor executor) {
